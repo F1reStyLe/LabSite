9998ebd5612c5709b6e2843f129d402333bdb82a
F1reStyLeJob <iwgw@bk.ru>
2023-05-16 16:23:59 +0300

    admin register

--- lab/admin.py
+++ lab/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Base)
+class AdminBase(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PigmentGroup)
+class AdminPigmentGroup(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Pigment)
+class AdminPigment(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PigmentPaste)
+class AdminPigmentPaste(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Enamel)
+class AdminEnamel(admin.ModelAdmin):
+    pass
