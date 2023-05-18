bdab9123c9ed5d749ea768e351cd0b720dee8098
F1reStyLeJob <iwgw@bk.ru>
2023-05-18 15:02:59 +0300

    NEW to master

--- lab/views.py
+++ lab/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import *
 
 def index(request):
-    enamels = Enamel.objects.all()
-    return render(request, template_name='lab/index.html', context={'enamels': enamels})
+
+    return render(request, template_name='lab/index.html')
