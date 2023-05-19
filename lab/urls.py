624c3e2c7152a4cfb51b88dd3ddbcff51075a770
F1reStyLeJob <iwgw@bk.ru>
2023-05-19 16:21:59 +0300

    add templates

--- lab/urls.py
+++ lab/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sample_list/', views.index, name='sample_list'),
+    path('add_sample/', views.index, name='add_sample'),
+    path('contacts/', views.index, name='contacts'),
+    path('sample/<str:pk>', views.index, name='contacts'),
 ]
--- lab/views.py
+++ lab/views.py
@@ -4,3 +4,19 @@ from .models import *
 def index(request):
 
     return render(request, template_name='lab/index.html')
+
+
+def sample_list(request):
+    samples = PigmentPaste.objects.all()
+    return render(request, template_name='lab/sample_list', context={'samples':samples})
+
+
+def sample(request, pk):
+    sample = PigmentPaste.objects.get(id=pk)    
+    return render(request, template_name='lab/sample', context={'sample': sample})
+
+
+def contacts(request):
+    
+    return render(request, template_name='lab/contacts')
+
