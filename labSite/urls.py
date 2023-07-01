d2e54fdf9aa559bd9708f728d20a18a36e203014
F1reStyLe <iwgw@bk.ru>
2023-07-01 22:19:46 +0300

    add Userserializer

--- labSite/urls.py
+++ labSite/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lab.urls')),
     path('api/', include('api.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
